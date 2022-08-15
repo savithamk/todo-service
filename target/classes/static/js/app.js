90a55284ceafd35a902b2b957f16b14fb92289a4
savithamk <savitha.maiya1@gmail.com>
2022-08-15 13:51:46 -0400

    Created front end pages

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -9,6 +9,10 @@ myApp.config(function($routeProvider,$locationProvider){
         templateUrl:'pages/main.html',
         controller:'mainController'
     })
+    .when('/:filt',{
+        templateUrl:'pages/main.html',
+        controller:'mainController'
+    })
 });
 
 myApp.config(['$httpProvider', function($httpProvider) {
@@ -19,6 +23,61 @@ myApp.config(['$httpProvider', function($httpProvider) {
 
 
 //Controllers
-myApp.controller('mainController',['$scope','$http',function($scope,httpService){
-    
+myApp.controller('mainController',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
+    $scope.newactivity='';
+        $scope.serviceURL="todo/"
+        $scope.filt= $routeParams.filt || 'all';
+
+        $scope.getAll=function(){
+            $http({
+                method:'GET',
+                url:$scope.serviceURL+$scope.filt,
+                dataType: 'json'}).then(function successCallback(response) {
+                $scope.activities=response.data;
+            }, function errorCallback(response) {
+
+            });
+        }
+
+        $scope.getAll();
+
+        //method to update status
+        $scope.changeStatus=function(id,name,status){
+            $http({
+                method:'PUT',
+                url:$scope.serviceURL+'update',
+                data:{id:id,
+                      name:name,
+                      status:status},
+                dataType: 'json'}).then(function successCallback(response) {
+                $scope.activities=response.data;
+            }, function errorCallback(response) {
+            });
+        }
+
+        //method to validate and add new activity
+        $scope.validateadd=function(){
+            if($scope.newactivity!='' && $scope.newactivity!=undefined && $scope.newactivity.length>0){
+                $http({
+                    method:'POST',
+                    url:$scope.serviceURL,
+                    data:{name:$scope.newactivity},
+                    dataType: 'json'}).then(function successCallback(response) {
+                    $scope.newactivity='';
+                    $scope.activities=response.data;
+                }, function errorCallback(response) {
+                });
+            }
+        }
+
+        //method to delete
+        $scope.deleteactivity=function(activityId){
+            $http({
+                method:'DELETE',
+                url:$scope.serviceURL+activityId})
+                .then(function successCallback(response) {
+                $scope.activities=response.data;
+            }, function errorCallback(response) {
+            });
+        }
 }]);
